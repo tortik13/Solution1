b45be7d90cd568f9dbee562b3c68f1b6289fb224
danielyan-s <94621829+danielyan-s@users.noreply.github.com>
2022-02-08 17:29:53 +0300

    Delete oxr.py

--- oxr.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(path):
-    load_dotenv(path)
-
-    APP_KEY = os.environ.get('APP_KEY')
